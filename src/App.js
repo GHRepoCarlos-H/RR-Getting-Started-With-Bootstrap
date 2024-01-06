ee94e1fe7eea096349c93a799bfde29ed5a9807d
Carlos Hernandez <chernandezmiranda@gmail.com>
2024-01-06 10:22:17 -0800

    Added multiple variants

--- src/App.js
+++ src/App.js
@@ -1,11 +1,156 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
+import Table from 'react-bootstrap/Table'
+import Spinner from 'react-bootstrap/Spinner'
+import Carousel from 'react-bootstrap/Carousel'
+import ExampleCarouselImage from './components/ExampleCarouselImage.jpeg';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Bootstrap Website</h1>
+      <h1>Hello World</h1>
+      <button type="button" class="btn btn-primary btn-lg">Blue Button</button>
+      
+      <Nav activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="/about">About Us</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+      <>
+      <Spinner animation="border" variant="primary" />
+      <Spinner animation="border" variant="secondary" />
+      <Spinner animation="border" variant="success" />
+      <Spinner animation="border" variant="danger" />
+      <Spinner animation="border" variant="warning" />
+      <Spinner animation="border" variant="info" />
+      <Spinner animation="border" variant="light" />
+      <Spinner animation="border" variant="dark" />
+      <Spinner animation="grow" variant="primary" />
+      <Spinner animation="grow" variant="secondary" />
+      <Spinner animation="grow" variant="success" />
+      <Spinner animation="grow" variant="danger" />
+      <Spinner animation="grow" variant="warning" />
+      <Spinner animation="grow" variant="info" />
+      <Spinner animation="grow" variant="light" />
+      <Spinner animation="grow" variant="dark" />
+    </>
+
+<Form>
+    <Form.Group controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+    </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+
+    <Form.Group controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    
+    <Button variant="primary" type="submit">
+      Submit
+    </Button>
+    <Form.Group>
+        <Form.Label>Form Dropdown with favorite movies</Form.Label>
+        <Form.Control as="select">
+          <option>Inception</option>
+          <option>Forrest Gump</option>
+          <option>Shawshank Redemption</option>
+          <option>Spiderman: Into the Spiderverse</option>
+          <option>Avengers: Endgame</option>
+        </Form.Control>
+     </Form.Group>
+  </Form>
+     <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+
+
+<Table responsive>
+      <thead>
+        <tr>
+          <th>#</th>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <th key={index}>Table heading</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))}
+        </tr>
+        <tr>
+          <td>2</td>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))}
+        </tr>
+        <tr>
+          <td>3</td>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <td key={index}>Table cell {index}</td>
+          ))}
+        </tr>
+      </tbody>
+    </Table>
+    <Carousel fade>
+      <Carousel.Item>
+        <ExampleCarouselImage text="ExampleCarouselImage.jpeg" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+
      
     </div>
   );
